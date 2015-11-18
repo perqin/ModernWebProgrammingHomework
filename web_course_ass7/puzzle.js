69822487872d85044b234b9c373708dc1ad004f2
Perqin <perqinxie@gmail.com>
2015-11-19 01:08:28 +0800

    working on settings

--- web_course_ass7/puzzle.js
+++ web_course_ass7/puzzle.js
@@ -2,9 +2,84 @@ var GameStateEnum = {
     GS_LOADING: "GS_LOADING",
     GS_PLAYING: "GS_PLAYING",
     GS_STOP: "GS_STOP"
-}, UNMOVEABLE = -1, ANIMATION_DURATION = 500, RANDOM_TIMES = 1000,
-    GameState, TimeUsed, Timer, FullPuzzleImage, EmptyTilePosition, MatchedCount,
-    PuzzleBorad, Tiles, RankingsButton, GameControlButton, SettingsButton, GameControlLabel;
+}, Boards = {
+    SETTINGS: "SETTINGS",
+    PUZZLE: "PUZZLE",
+    RANKINGS: "RANKINGS",
+    LOADING: "LOADING"
+}, Lang = ["CN", "EN"], LngSrc = [
+    {
+        settings: "设置",
+        back: "返回",
+        language: "简体中文"
+    },
+    {
+        settings: "SETTINGS",
+        back: "BACK",
+        language: "English"
+    }
+],
+    UNMOVEABLE = -1, ANIMATION_DURATION = 500, RANDOM_TIMES = 1000,
+    GameState, CurrentBoard, TimeUsed, Timer, FullPuzzleImage, EmptyTilePosition, MatchedCount, LanguageIndex, PuzzleIndex,
+    PuzzleBoard, SettingsBoard, LoadingScreen, Tiles, RankingsButton, GameControlButton, SettingsButton, RankingsLabel, GameControlLabel, SettingsLabel, LanguageLabel, PrevLanguageButton, NextLanguageButton, prevPuzzleButton, NextPuzzleButton;
+
+// Utils function - get translation
+function tr(key) {
+    "use strict";
+    return LngSrc[LanguageIndex][key];
+}
+
+// Utils function - send http request and parse returned JSON into object
+function sendHttpRequest(method, url, data, onSucceed, onFail) {
+    "use strict";
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        switch (xhr.readyState) {
+        case xhr.OPENED:
+            break;
+        case xhr.HEADERS_RECEIVED:
+            if (xhr.status !== 200) {
+                window.console.log("Connection Error with code " + xhr.status + " : " + xhr.statusText);
+                onFail();
+            }
+            break;
+        case xhr.DONE:
+            if (xhr.status === 200) {
+                try {
+                    onSucceed(JSON.parse(xhr.responseText));
+                } catch (err) {
+                    window.console.log("Loading error.");
+                    onFail();
+                }
+            } else {
+                window.console.log("Error.");
+                onFail();
+            }
+            break;
+        }
+
+    };
+    if (method === "GET") {
+        xhr.open("GET", url);
+        xhr.send();
+    }
+    if (method === "POST") {
+        xhr.open("POST", url);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.setRequestHeader("Content-Length", data.length);
+        xhr.send(data);
+    }
+}
+
+function showLoadingScreen() {
+    "use strict";
+    LoadingScreen.className = "board-visible board";
+}
+
+function hideLoadingScreen() {
+    "use strict";
+    LoadingScreen.className = "board-hidden board";
+}
 
 function randomizePosition() {
     "use strict";
@@ -82,6 +157,8 @@ function startGame() {
     repositionTiles();
     GameState = GameStateEnum.GS_PLAYING;
     GameControlLabel.textContent = "STOP";
+    RankingsButton.className = "button button-main button-disable button-main-l";
+    SettingsButton.className = "button button-main button-disable button-main-r";
 }
 
 function stopGame() {
@@ -89,6 +166,8 @@ function stopGame() {
     setTilesBack();
     GameState = GameStateEnum.GS_STOP;
     GameControlLabel.textContent = "START";
+    RankingsButton.className = "button button-main button-enable button-main-l";
+    SettingsButton.className = "button button-main button-enable button-main-r";
 }
 
 function startNewGame() {
@@ -97,6 +176,60 @@ function startNewGame() {
         startGame();
     } else {
         stopGame();
+    }
+}
+
+function updateLanguage() {
+    "use strict";
+    // TODO
+}
+
+function prevLanguage() {
+    "use strict";
+    LanguageIndex -= 1;
+    PrevLanguageButton.className = "button button-prev button-" + (LanguageIndex === 0 ? "disable" : "enable");
+    NextLanguageButton.className = "button button-next button-" + (LanguageIndex === Lang.length - 1 ? "disable" : "enable");
+    updateLanguage();
+}
+
+function nextLanguage() {
+    "use strict";
+    LanguageIndex += 1;
+    PrevLanguageButton.className = "button button-prev button-" + (LanguageIndex === 0 ? "disable" : "enable");
+    NextLanguageButton.className = "button button-next button-" + (LanguageIndex === Lang.length - 1 ? "disable" : "enable");
+    updateLanguage();
+}
+
+function loadSettings() {
+    "use strict";
+    showLoadingScreen();
+    LanguageLabel.textContent = tr("language");
+    PrevLanguageButton.className = "button button-prev button-" + (LanguageIndex === 0 ? "disable" : "enable");
+    NextLanguageButton.className = "button button-next button-" + (LanguageIndex === Lang.length - 1 ? "disable" : "enable");
+    // TODO
+    hideLoadingScreen();
+}
+
+function openSettings() {
+    "use strict";
+    if (GameState !== GameStateEnum.GS_STOP) {
+        return;
+    }
+    if (CurrentBoard === Boards.SETTINGS) {
+        CurrentBoard = Boards.PUZZLE;
+        SettingsBoard.className = "board-hidden board";
+        RankingsButton.className = "button button-main button-enable button-main-l";
+        GameControlButton.className = "button button-main button-enable button-main-m";
+        SettingsButton.className = "button button-main button-enable button-main-r";
+        SettingsLabel.textContent = tr("settings");
+    } else {
+        CurrentBoard = Boards.SETTINGS;
+        SettingsBoard.className = "board-visible board";
+        RankingsButton.className = "button button-main button-disable button-main-hide";
+        GameControlButton.className = "button button-main button-disable button-main-hide";
+        SettingsButton.className = "button button-main button-enable button-main-long";
+        SettingsLabel.textContent = tr("back");
+        loadSettings();
     }
 }
 
@@ -157,7 +290,9 @@ function initElementsAndRt() {
     "use strict";
     var i, tile, t, l;
     Tiles = [];
-    PuzzleBorad = document.getElementById("puzzle-board");
+    PuzzleBoard = document.getElementById("puzzle-board");
+    SettingsBoard = document.getElementById("settings-board");
+    LoadingScreen = document.getElementById("loading-overlay");
     for (i = 0; i < 15; i += 1) {
         tile = document.createElement("canvas");
         tile.className = "tile";
@@ -171,23 +306,42 @@ function initElementsAndRt() {
         tile.style.left = l.toString() + "px";
         tile.position = i;
         tile.onclick = tileClicked;
-        PuzzleBorad.appendChild(tile);
+        PuzzleBoard.appendChild(tile);
         Tiles.push(tile);
     }
     RankingsButton = document.getElementById("game-rankings");
+    RankingsLabel = document.getElementById("game-rankings-label");
     GameControlButton = document.getElementById("game-control");
     GameControlLabel = document.getElementById("game-control-label");
     SettingsButton = document.getElementById("game-settings");
+    SettingsLabel = document.getElementById("game-settings-label");
+    LanguageLabel = document.getElementById("language-label");
+    PrevLanguageButton = document.getElementById("prev-lang");
+    NextLanguageButton = document.getElementById("next-lang");
     FullPuzzleImage = new Image();
     FullPuzzleImage.onload = fullPuzzleLoaded;
     EmptyTilePosition = 15;
     MatchedCount = 15;
+    LanguageIndex = 0;
     GameState = GameStateEnum.GS_STOP;
+    CurrentBoard = Boards.PUZZLE;
+}
+
+function onPMouseDown(event) {
+    "use strict";
+    event.preventDefault();
 }
 
 function dispatchEvents() {
     "use strict";
+    var ps, i;
     GameControlButton.onclick = startNewGame;
+    SettingsButton.onclick = openSettings;
+    // Prevent double-click text selection
+    ps = document.getElementsByTagName("p");
+    for (i = 0; i < ps.length; i += 1) {
+        ps[i].addEventListener("mousedown", onPMouseDown);
+    }
 }
 
 window.onload = function () {
